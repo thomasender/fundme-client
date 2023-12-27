86f60f0835bf5000d92803bd5633d2170eb967ef
thomasender <freisingen@yahoo.com>
2023-12-27 23:11:44 +0100

    add PROD Contract address

--- src/constants.ts
+++ src/constants.ts
@@ -8,8 +8,9 @@ import { ContractABI } from "./hooks/use-contract";
 // Contract at this address has minusd value of 5000 wei for easier testing, getting test Matic is hard
 // "0x6DF45b871ab0db23b2a1e631FB39b78A6ec3E60f";
 // FUND ME FOR TESTING Contract on MAINNET: 0x7e140496Abc7227b0ae0081CBe8eeC399B2c86D6
+// FUND ME PROD ON MAINNET: 0xBADC8EEe035bE0f0b8B7f2eb63FD5a89ECB1b361
 // OWNER: 0x47c65A5A41cB8f369c053CaB475708e065637b82
-export const FUND_ME_ADDRESS = "0x7e140496Abc7227b0ae0081CBe8eeC399B2c86D6";
+export const FUND_ME_ADDRESS = "0xBADC8EEe035bE0f0b8B7f2eb63FD5a89ECB1b361";
 export const CONTRACT_ABI = ABI_PROD as ContractABI;
 // Mumbai Testnet TX BASE URL: https://mumbai.polygonscan.com/tx/
 // Mainnet TX BASE URL: https://polygonscan.com/tx/
